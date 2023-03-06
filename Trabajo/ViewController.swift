db99b0616aa5b4b0dcc13573e2e62d6e36d056f7
Arantza <aer.castro.dessavre@gmail.com>
2023-03-06 12:12:50 -0600

    Cambie texto

--- Trabajo/ViewController.swift
+++ Trabajo/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Texto: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Botón(_ sender: Any) {
+        Texto.text = "Oprimiste el boton"
+    }
+    
 }
 
